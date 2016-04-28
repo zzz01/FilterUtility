9a21ac41d013104df1a36ad08f303e8002d87ba2
gaoyan601 <347259020@qq.com>
2016-04-28 10:27:12 +0800

    New project

---
+++ Filter/src/main/java/Hust/Kevin/Filter/App.java
@@ -0,0 +1,13 @@
+package Hust.Kevin.Filter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
