3461069ca97442ac0d2d5277edd4b7750f5adbe2
gaoyan601 <347259020@qq.com>
2016-06-03 14:30:28 +0800

    在工具包util中新建time类，其中有两个静态方法，一个是将其它日期格式转为标准日期格式，另一个是对比给定的两个时间的大小。

---
+++ Filter/src/main/java/gov/sc/utils/Time.java
@@ -0,0 +1,12 @@
+package gov.sc.utils;
+
+public class Time {
+
+	public static String convertToStandar(String time) {
+		return null;
+	}
+
+	public static int compareToTime(String t1, String t2) {
+		return 0;
+	}
+}
